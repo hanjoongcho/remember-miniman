7caeaa86c37fcb708352d816ca63da5b619f9d6c
hanjoong.cho <hanjoongcho@gmail.com>
2017-11-13 01:08:32 +0900

    Change launcher icon style

---
+++ app/src/test/java/me/blog/korn123/rememberminiman/KotlinBasicSyntaxText.kt
@@ -0,0 +1,14 @@
+package me.blog.korn123.rememberminiman
+
+import org.junit.Test
+
+/**
+ * Created by CHO HANJOONG on 2017-08-25.
+ */
+class Tests {
+
+    fun main() {
+        print("Hello Kotlin")
+    }
+
+}
